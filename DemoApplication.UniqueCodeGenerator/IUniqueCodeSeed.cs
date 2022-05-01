039a5c6b53de6041c11a37af6b87d8d7117e5b2f
RichardvZyl <richardvzyl@gmail.com>
2022-05-01 20:44:51 +0200

    Moved UniqueCode seed to it's own repo

--- DemoApplication.UniqueCodeGenerator/IUniqueCodeSeed.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace DemoApplication.UniqueCodeGenerator;
-public interface IUniqueCodeSeed
-{
-    Task<bool> Start(IServiceCollection serviceCollection);
-}
