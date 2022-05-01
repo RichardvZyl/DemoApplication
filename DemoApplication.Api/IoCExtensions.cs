ea62d4bf04d5526e1b7642af0ac0c6488a3ad4f9
RichardvZyl <richardvzyl@gmail.com>
2022-05-01 09:16:48 +0200

    renamed extensions to add context

--- DemoApplication.Api/IoCExtensions.cs
+++ DemoApplication.Api/IoCExtensions.cs
@@ -1,42 +1,13 @@
-﻿using System.Linq;
-using Abstractions.AspNetCore;
+﻿using Abstractions.AspNetCore;
 using Abstractions.IoC;
 using DemoApplication.Database;
 using DemoApplication.Framework;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace DemoApplication.Api;
 
-/// <summary> Add versioning functionality </summary>
-public class RemoveVersionParameterFilter : IOperationFilter
-{
-    /// <summary> Add versioning functionality to Swagger </summary>
-    public void Apply(OpenApiOperation operation, OperationFilterContext context)
-    {
-        var versionParameter = operation.Parameters.Single(p => p.Name == "version");
-        _ = operation.Parameters.Remove(versionParameter);
-    }
-}
-
-/// <summary> Add current version </summary>
-public class ReplaceVersionWithExactValueInPathFilter : IDocumentFilter
-{
-    /// <summary> Add current version </summary>
-    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
-    {
-        var paths = new OpenApiPaths();
-        foreach (var path in swaggerDoc.Paths)
-        {
-            paths.Add(path.Key.Replace("v{version}", swaggerDoc.Info.Version), path.Value);
-        }
-        swaggerDoc.Paths = paths;
-    }
-}
-
 /// <summary> Add functionality to existing features </summary>
-public static class Extensions
+public static class IoCExtensions
 {
 
     /// <summary> Add Services to be used by application for dependency injection </summary>
