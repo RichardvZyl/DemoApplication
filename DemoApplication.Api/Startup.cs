4d4917a6e4eb91280490c99df0400c319552813f
RichardvZyl <richardvzyl@gmail.com>
2022-05-01 09:16:13 +0200

    Implemented abstracted Swagger

--- DemoApplication.Api/Startup.cs
+++ DemoApplication.Api/Startup.cs
@@ -1,20 +1,14 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Abstractions.AspNetCore;
+using Abstractions.EntityFrameworkCore;
+using Abstractions.Security;
+using Abstractions.SwaggerExtension;
+using DemoApplication.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using Abstractions.AspNetCore;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using Abstractions.EntityFrameworkCore;
-using DemoApplication.Database;
-using Abstractions.Security;
-using Abstractions.IoC;
-
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.Formatters;
-//using Microsoft.Extensions.Options;
 
 namespace DemoApplication.Api;
 
@@ -48,13 +42,7 @@ public class Startup
         application.UseAuthentication();
         application.UseAuthorization();
 
-        application.UseSwagger();
-        application.UseSwaggerUI(c =>
-        {
-            c.SwaggerEndpoint($"/swagger/v1/swagger.json", $"v1");
-            // Specify an endpoint for v2
-            c.SwaggerEndpoint($"/swagger/v2/swagger.json", $"v2");
-        });
+        application.AddSwagger();
 
         application.UseEndpoints();
     }
@@ -62,102 +50,25 @@ public class Startup
     /// <summary>  Configure application Services </summary>
     public void ConfigureServices(IServiceCollection services)
     {
-
         services.AddCors();
         services.AddSecurity();
-        services.AddCryptography(IoCExtensions.GetSecrets(services, "CryptoKey"));
+        services.AddCryptography(Abstractions.IoC.IoCExtensions.GetSecrets(services, "CryptoKey"));
         services.AddResponseCompression();
         services.AddResponseCaching();
         services.AddControllersDefault();
-        
+
         //services.AddContextUsePostgreSQL<Context>();
         //services.AddContextUseSQL<Context>();
         services.AddContextUseMemory<Context>();
-        
+
         services.AddApiVersioning();
-        services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "DemoApplicationAPI v1", Version = "v1" });
-            // Add a SwaggerDoc for v2 
-            c.SwaggerDoc("v2",
-                new OpenApiInfo
-                {
-                    Version = "v2",
-                    Title = "DemoApplicationAPI v1"
-                });
-            c.OperationFilter<RemoveVersionParameterFilter>();
-            c.DocumentFilter<ReplaceVersionWithExactValueInPathFilter>();
-            c.EnableAnnotations();
 
-            //c.DocInclusionPredicate((version, desc) =>
-            //{
-            //var versions = desc.ControllerAttributes()
-            //    .OfType<ApiVersionAttribute>()
-            //    .SelectMany(attr => attr.Versions);
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        services.AddSwagger(xmlPath);
 
-            //var maps = desc.ActionAttributes()
-            //    .OfType<MapToApiVersionAttribute>()
-            //    .SelectMany(attr => attr.Versions)
-            //    .ToArray();
-
-            //    return versions.Any(v => $"v{v.ToString()}" == version)
-            //                  && (!maps.Any() || maps.Any(v => $"v{v.ToString()}" == version)); ;
-            //});
-
-            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            c.IncludeXmlComments(xmlPath);
-
-            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                In = ParameterLocation.Header,
-                Description = "Please enter into field the word 'Bearer' following by space and JWT",
-                Name = "Authorization",
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer"
-            });
-
-            c.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        },
-                        Scheme = "oauth2",
-                        Name = "Bearer",
-                        In = ParameterLocation.Header,
-
-                    },
-                    new List<string>()
-                }
-            });
-        });
-
-        services
-            .AddControllersWithViews()
-            .AddNewtonsoftJson();
+        services.AddControllersWithViews();
 
         services.AddServices();
     }
-
-    /// <summary> JSonPatch Input Formatter </summary>
-    ////private static NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter()
-    ////{
-    ////    var builder = new ServiceCollection()
-    ////        .AddLogging()
-    ////        .AddMvc()
-    ////        .AddNewtonsoftJson()
-    ////        .Services.BuildServiceProvider();
-
-    ////    return builder
-    ////        .GetRequiredService<IOptions<MvcOptions>>()
-    ////        .Value
-    ////        .InputFormatters
-    ////        .OfType<NewtonsoftJsonPatchInputFormatter>()
-    ////        .First();
-    ////}
 }
